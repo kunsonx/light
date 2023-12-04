068b0978e50708327fb743f6476b1e0e90827929
jon <jon@jons-Mac-Pro-7.local>
2023-12-05 00:51:44 +0800

    fix : simply codes

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@
 #![allow(non_snake_case)]
 
 use std::ffi::CString;
-use std::os::raw::c_char;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 fn main() {
     unsafe {
         let c_string = CString::new("rust").expect("CString::new failed");
-        add(1, 2, c_string.into_raw() as *mut c_char);
+        add(1, 2, c_string.into_raw());
     }
     println!("Hello, world!");
 }
